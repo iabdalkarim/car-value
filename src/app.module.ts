f07bc6dc68ad512d29652f0f0f40d26a8aedda78
Islam Ahmed <islam.abdalkarim.hamed@gmail.com>
2026-09-08 18:56:08 +0300

    fix the app module by removing user controller from providers as it imported by UsersModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ReportsModule } from './reports/reports.module';
-import { UsersController } from './users/users.controller';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users/user.entity';
@@ -19,7 +18,7 @@ import { Report } from './reports/report.entity';
     ReportsModule,
     UsersModule,
   ],
-  controllers: [AppController, UsersController],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
