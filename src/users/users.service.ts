5b243f62211c273e4a7209e67e93f2ba2263d1c0
Islam Ahmed <islam.abdalkarim.hamed@gmail.com>
2026-09-10 09:17:05 +0300

    use Nest NotFoundException

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -27,7 +27,7 @@ export class UsersService {
   async update(id: number, attr: Partial<User>) {
     const user = await this.findOne(id);
     if (!user) {
-      throw new Error('User not found');
+      throw new NotFoundException('User not found');
     }
     Object.assign(user, attr);
     return this.userRepository.save(user);
@@ -36,7 +36,7 @@ export class UsersService {
   async remove(id: number) {
     const user = await this.findOne(id);
     if (!user) {
-      throw new Error('User not found');
+      throw new NotFoundException('User not found');
     }
     return this.userRepository.remove(user);
   }
